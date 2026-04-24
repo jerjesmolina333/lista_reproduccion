fe8d2e784efac34e14270675df53beab76ff1e09
jerjesmolina333 <jerjesmolina@gmail.com>
2026-04-24 09:59:32 -0600

    Sombra de la tarjeta principal y Enter para dar de alta canción

--- script.js
+++ script.js
@@ -22,11 +22,6 @@ function addSong(artistValue, titleValue) {
 
   songElement.querySelector(".song__artist").textContent = artistValue;
   songElement.querySelector(".song__title").textContent = titleValue;
-  songElement
-    .querySelector(".song__like")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("song__like_active");
-    });
   songsContainer.append(songElement);
 }
 
@@ -49,4 +44,25 @@ resetButton.addEventListener("click", function () {
   });
 
   renderNoSongs();
+});
+
+function keyHandler(evt) {
+  if (evt.key === "Enter") {
+    addSong(artistInput.value, titleInput.value);
+    renderHasSongs();
+
+    artistInput.value = "";
+    titleInput.value = "";
+  }
+}
+
+const artistInput = document.querySelector(".input__text_type_artist");
+const titleInput = document.querySelector(".input__text_type_title");
+
+artistInput.addEventListener("keydown", keyHandler);
+
+songsContainer.addEventListener("click", function (evt) {
+  if (evt.target.classList.contains("song__like")) {
+    evt.target.classList.toggle("song__like_active");
+  }
 });
